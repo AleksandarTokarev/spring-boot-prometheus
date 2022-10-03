392fb65718dc29a55e81e5d4a1b9a3ce3e3cade9
Aleksandar Tokarev <a.tokarev90@gmail.com>
2022-10-03 15:29:05 +0200

    Add Tags or Filter by Tags

--- src/main/java/com/tokarev/prometheus/configuration/CustomMetricsConfig.java
+++ src/main/java/com/tokarev/prometheus/configuration/CustomMetricsConfig.java
@@ -1,5 +1,6 @@
 package com.tokarev.prometheus.configuration;
 
+import io.micrometer.core.instrument.ImmutableTag;
 import io.micrometer.core.instrument.Meter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.config.MeterFilter;
@@ -14,10 +15,10 @@ public class CustomMetricsConfig {
     public MeterRegistryCustomizer<MeterRegistry> metricsToExpose() {
         return registry -> registry
                 .config()
-                .meterFilter(MeterFilter.denyUnless(this::isMetricToInclude));
+                .meterFilter(MeterFilter.denyUnless(this::isMetricToInclude)).commonTags("a_tag", "some_common_tags"); // Add tags for all or specific metrics
     }
 
     private boolean isMetricToInclude(Meter.Id id) {
-        return id.getName().startsWith("app") || id.getName().startsWith("http");
+        return (id.getName().startsWith("app") || id.getName().startsWith("http")) && id.getTags().contains(new ImmutableTag("outcome", "SUCCESS"));  // Filter by tags
     }
 }
